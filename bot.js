139214adbd5cf8a4151fd8242ad932e26fbec067
MorningLit <ambroseliew1998@gmail.com>
2020-06-09 11:16:47 +0800

    Add silence feature

--- bot.js
+++ bot.js
@@ -1,32 +1,46 @@
 const Discord = require("discord.js");
 const checkWord = require("check-word");
 const emojiStrip = require("emoji-strip");
+const stripChar = require("stripchar").StripChar;
 
 require("dotenv").config();
 const words = checkWord("en");
 const client = new Discord.Client();
 const token = process.env.TOKEN;
 const prefix = "j!";
+
 var retarded = false;
 var twoWords = true;
+var silenced = false;
 
 client.on("ready", () => {
   console.log("I am online!");
 });
 
 client.on("message", (msg) => {
-  if (!msg.author.bot) {
-    // must not be a bot
+  if (silenced && msg.content === `${prefix}unsilence`) {
+    silenced = false;
+    return msg.channel.send("I am back!");
+  }
+
+  if (!silenced && !msg.author.bot) {
+    // must not be silenced and not from a bot
     var arr = emojiStrip(msg.content).split(" ");
 
-    if (arr.length === 1) {
+    if (arr.length === 1 && arr[0].substring(0, 2) === `${prefix}`) {
       // check prefix only
-      if (arr[0] === `${prefix}retarded`) {
+      var restStr = arr[0].substring(2);
+      if (restStr === `silence`) {
+        silenced = true;
+        return msg.channel.send(`SHUTUP JASON`);
+      }
+      if (restStr === `retarded`) {
         retarded = !retarded;
         return retarded
-          ? msg.channel.send(`I am now yason 🥴`)
-          : msg.channel.send(`I am now JASON 💪`);
-      } else if (arr[0] === `${prefix}twoWords`) {
+          ? msg.channel.send(`I am now jason 🥴`)
+          : msg.channel.send(`I am now YASON 💪`);
+      }
+      if (restStr === `twoWords`) {
         twoWords = !twoWords;
         return twoWords
           ? msg.channel.send(`scanning only 2 words`)
@@ -41,6 +55,7 @@ client.on("message", (msg) => {
       arr = find1_2Longest(arr);
       console.log("STEP 1: " + arr);
     }
+
     if (arr.length === 2) {
       if (retarded && arr[0] !== arr[1]) {
         // switches first letter of the 2 words
@@ -55,11 +70,16 @@ client.on("message", (msg) => {
         }
         return msg.channel.send(`${newFirstWord} ${newSecondWord}`);
       }
+
       if (!retarded) {
         // msg must be 2 words
         var firstWord = arr[0].toLowerCase();
         var secondWord = arr[1].toLowerCase();
-
+        console.log(
+          `${firstWord}: ${words.check(firstWord)} ${secondWord}: ${words.check(
+            secondWord
+          )}`
+        );
         if (
           (!words.check(firstWord) || !words.check(secondWord)) &&
           firstWord !== secondWord
@@ -81,8 +101,8 @@ client.on("message", (msg) => {
               // found an actual word after mixmatching
               if (!twoWords) {
                 let newSentence = msg.content
-                  .replace(firstWord, newFirstWord)
-                  .replace(secondWord, newSecondWord);
+                  .replace(arr[0], newFirstWord)
+                  .replace(arr[1], newSecondWord);
                 return msg.channel.send(newSentence);
               }
               return msg.channel.send(`${newFirstWord} ${newSecondWord}`);
@@ -99,7 +119,6 @@ function emoteMentionLinkChecker(str) {
   let firstCharLastChar = str.charAt(0) + str.charAt(length - 1);
   if (firstCharLastChar === "<>") return true;
 
-  str.replace(/[^a-zA-Z ]/g, "");
   return str.substring(0, 4) === "http" || str === "";
 }
 
@@ -109,11 +128,13 @@ function removeEmoteMentionLink(arr) {
   let newArr = [];
   for (var i = 0; i < num; i++) {
     if (!emoteMentionLinkChecker(arr[i])) {
+      arr[i] = stripChar.RSExceptAlpha(arr[i]);
       newArr.push(arr[i]);
     }
   }
-  console.log("AFTER: " + newArr);
-  return newArr;
+  var filtered = newArr.filter(Boolean);
+  console.log(`AFTER: ${newArr} FILTER: ${filtered}`);
+  return filtered;
 }
 
 function find1_2Longest(arr) {
